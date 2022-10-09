f95b20d6a84ba49406d23f989d30ec044b12d39d
henrdeld <henry8222@gmail.com>
2022-10-08 20:01:10 -0500

    Cambio respuesta de codigos en libraryCOntroller ResponseStatus(HttpStatus.CREATED)

--- src/main/java/com/usa/misiontic/MasterClass3/controller/LibraryController.java
+++ src/main/java/com/usa/misiontic/MasterClass3/controller/LibraryController.java
@@ -5,6 +5,7 @@ import com.usa.misiontic.MasterClass3.entities.Category;
 import com.usa.misiontic.MasterClass3.entities.Library;
 import com.usa.misiontic.MasterClass3.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class LibraryController {
     }
 
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Library save(@RequestBody Library p){
         return libraryService.save(p);
     }
